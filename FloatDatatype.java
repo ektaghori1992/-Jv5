e8ff34a67db2747b6884ff523234672f59605c4e
ekta <aektapatel0102@gmail.com>
2022-07-20 13:48:35 +0100

    my first program

---
+++ FloatDatatype.java
@@ -0,0 +1,14 @@
+public class FloatDatatype {
+    public static void main(String[] args) {
+        float E, G;
+        E = 3.44f;
+        G = 2.33f;
+        System.out.println(E + G);
+        System.out.println(E - G);
+        System.out.println(E * G);
+        System.out.println(E / G);
+
+
+
+    }
+    }
